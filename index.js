8acd02f90d267abbbf78bc21f7dbe965a5373118
javiercolv <javiercolv@gmail.com>
2025-07-30 12:13:08 -0600

    feat: Update input parameters

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const core=require('@actions/core');
 try {
-  const name = core.getInput('name');
-  if (!name) {
-    throw new Error('Input is required');
+  const name = core.getInput('nombre');
+  const surname = core.getInput('apellido');
+  const age = core.getInput('edad');
+  if (!nombre || !surname || !age) {
+    throw new Error('Inputs "nombre", "apellido", and "edad" are required.');
   }
   //core.setOutput('output', `Processed: ${input}`);
-  console.log(`Hola, ${name}!`);
+  console.log(`Hola ${name} ${surname}, tienes ${age} años.`);
 } catch (error) {
   core.setFailed(`Action failed with error: ${error.message}`);
 }
