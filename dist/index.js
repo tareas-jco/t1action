7b90daae43f328064c0d3220cca39d8aaf084914
javiercolv <javiercolv@gmail.com>
2025-07-30 15:36:55 -0600

    fix: Remove input validation for name, surname, and age in index.js

--- dist/index.js
+++ dist/index.js
@@ -27560,9 +27560,6 @@ try {
   const name = core.getInput('nombre');
   const surname = core.getInput('apellido');
   const age = core.getInput('edad');
-  if (!name || !surname || !age) {
-    throw new Error('Inputs "nombre", "apellido", and "edad" are required.');
-  }
   //core.setOutput('output', `Processed: ${input}`);
   console.log(`Hola ${name} ${surname}, tienes ${age} años.`);
 } catch (error) {
