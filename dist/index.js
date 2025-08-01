437997a589402481f342d6dfd6e5d99a8ddfe438
javiercolv <javiercolv@gmail.com>
2025-08-01 09:55:27 -0600

    feat: Add input validation and special case handling for "Chilaquil" in age calculation

--- dist/index.js
+++ dist/index.js
@@ -27560,12 +27560,19 @@ try {
   const nombre = core.getInput('nombre');
   const apellido = core.getInput('apellido');
   const edad = core.getInput('edad');
-  //if (!nombre || !apellido || !edad) {
-  //  throw new Error('Inputs "nombre", "apellido", and "edad" are required.');
-  //}
-  core.info(` imprime  ${nombre} / ${apellido} / ${edad}...`);
-  //core.setOutput('output', `Processed: ${input}`);
-  //console.log(`Hola ${name} ${surname}, tienes ${age} años.`);
+  if (!nombre || !apellido || !edad) {
+    throw new Error('Inputs "nombre", "apellido", and "edad" are required.');
+  }
+  if (nombre == "Chilaquil") {
+    core.info(`El año es bisiesto.`);
+    const intEdad = parseInt(edad);
+    const anioActual = new Date().getFullYeßar();
+    const anioNacimiento = anioActual - intEdad * 4;
+    const cienAnios = anioNacimiento + 400;
+    core.info(`${nombre} ${apellido} nació el 29 de febrero de ${anioNacimiento} y cumplirá 100 años en el año ${cienAnios}.`); 
+  } else{
+    core.info(`${nombre} ${apellido} tiene ${edad} años de edad.`);
+  }
 } catch (error) {
   core.setFailed(`Action failed with error: ${error.message}`);
 }
